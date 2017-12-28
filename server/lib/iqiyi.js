1a762fbb5f036e6b432e49f10ae0394b7a8f7664
iwroku <veganQian@163.com>
2017-12-28 17:36:56 +0800

    增加iqiyi视频获取接口

--- server/lib/iqiyi.js
+++ server/lib/iqiyi.js
@@ -4,4 +4,21 @@
  * @date    2017-12-28 16:05:57
  * @version 1.0
  */
+const cheerio = require('cheerio')
+const axios = require('axios')
 
+// 电视剧
+axios.get('http://list.iqiyi.com/www/2/----------2-2017--4-1-1-iqiyi--.html').then(res => {
+	let $ = cheerio.load(res.data)
+	console.log($('ul.site-piclist').html())
+}).catch(e => {
+	console.log(e)
+})
+
+// 电影
+axios.get('http://list.iqiyi.com/www/1/----------2-2017--11-1-1-iqiyi--.html').then(res => {
+	let $ = cheerio.load(res.data)
+	console.log($('ul.site-piclist').find('li').eq(0).text())
+}).catch(e => {
+	console.log(e)
+})
