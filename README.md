b21de08662ed1057e11dd72762a5f09b1654d72e
iwroku <veganQian@163.com>
2017-12-28 14:30:28 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vip_video
-解析各大网站VIP视频接口
+# 解析各大视频网站vip电视剧及电影
+
+> 
+``` bash
+# 使用技术
+mongo+vue+vuex
+```
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="page">
+  	<video src="https://vodcn.iworku.com/Bingfen-Nongchang-1-x264-1025.mp4"></video>	
+  	<!-- <my-video :sources="video.sources" :options="video.options"></my-video> -->
+  </div>  
+</template>
+
+<script>
+	import myVideo from 'vue-video'
+	export default {
+		components: {
+			myVideo
+		},
+		data() {
+		  return { 
+		  	video: {
+          sources: [{
+            src: 'https://vodcn.iworku.com/Bingfen-Nongchang-1-x264-1025.mp4',
+            type: 'video/mp4'
+          }],
+          options: {
+            autoplay: true,
+            volume: 0.6,
+            poster: 'http://covteam.u.qiniudn.com/poster.png'
+          }
+	      }
+		  }
+		},  
+		computed: {
+		},    
+		created() {
+		},
+		methods: {
+		}
+	}
+</script>
+
+<style lang='scss'>
+    
+</style>
